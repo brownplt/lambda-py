1ccdb8e75ea786ed2cf1d24667b6b48a7d8ae07c
Yao Zhang <yao_zhang@brown.edu>
2012-12-18 22:58:24 -0500

    redex: add reduction rules for let and id

--- redex/lambdapy-reduction.rkt
+++ redex/lambdapy-reduction.rkt
@@ -1,6 +1,7 @@
 #lang racket
 
 (require redex)
+(require redex/tut-subst)
 (require "lambdapy-core.rkt")
 
 (define red
@@ -27,24 +28,41 @@
    (--> (in-hole P (seq val e))
         (in-hole P e)
         "seq")
+   (--> ((in-hole E (let (x_1 val) e))
+         (ε_1 ε ...)
+         Σ)
+        ((in-hole E (subst (x_1) (x_new) e))
+         ((extend-env ε_1 x_new ref_new) ε ...)
+         (override-store Σ ref_new val))
+        (fresh x_new)
+        (where ref_new ,(new-loc))
+        "let")
+   (--> ((in-hole E (id x_1 local))
+         (name env (((x_2 ref_2) ... (x_1 ref_1) (x_3 ref_3) ...) ε ...))
+         (name store ((ref_4 val_4) ... (ref_1 val_1) (ref_5 val_5) ...)))
+        ((in-hole E val_1)
+         env
+         store)
+        (side-condition (not (member (term x_1) (term (x_2 ... x_3 ...)))))
+        (side-condition (not (member (term ref_1) (term (ref_4 ... ref_5 ...)))))
+        (side-condition (not (redex-match? λπ (undefined-val) (term val_1)))) ;; TODO: exception for undefined
+        "id-local")
    (--> ((in-hole E (assign (id x_1 local) val_1))
-         ((name scope ((x_2 ref) ...)) ε ...)
-         (name store ((ref_2 val) ...)))
+         ((name scope ((x_2 ref_2) ...)) ε ...)
+         Σ)
         ((in-hole E vnone)
          ((extend-env scope x_1 ref_1) ε ...)
-         (override-store store ref_1 val_1))
+         (override-store Σ ref_1 val_1))
         (side-condition (not (member (term x_1) (term (x_2 ...)))))
-        (side-condition (not (member (term ref_1) (term (ref_2 ...)))))
         (where ref_1 ,(new-loc))
         "assign-local-free")
    (--> ((in-hole E (assign (id x_1 local) val_1))
-         (name env (((x_2 ref) ... (x_1 ref_1) (x_3 ref) ...) ε ...))
-         (name store ((ref_2 val) ... (ref_1 val_0) (ref_3 val) ...)))
+         (name env (((x_2 ref_2) ... (x_1 ref_1) (x_3 ref_3) ...) ε ...))
+         Σ)
         ((in-hole E vnone)
          env
-         (override-store store ref_1 val_1))
+         (override-store Σ ref_1 val_1))
         (side-condition (not (member (term x_1) (term (x_2 ... x_3 ...)))))
-        (side-condition (not (member (term ref_1) (term (ref_2 ... ref_3 ...)))))
         "assign-local-bound")
    ))
 
@@ -56,7 +74,7 @@
 
 (define-metafunction λπ
   extend-env : ε x ref -> ε
-  [(extend-env ((x_1 ref_1) ...) x ref) ((x_1 ref_1) ... (x ref))])
+  [(extend-env ((x_2 ref_2) ...) x_1 ref_1) ((x_2 ref_2) ... (x_1 ref_1))])
 
 (define-metafunction λπ
   override-store : Σ ref val -> Σ
@@ -66,6 +84,11 @@
   [(override-store ((ref_2 val_2) ... (ref_1 val_0) (ref_3 val_3) ...) ref_1 val_1)
    ((ref_2 val_2) ... (ref_1 val_1) (ref_3 val_3) ...)
    (side-condition (not (member (term ref_1) (term (ref_2 ...)))))])
+
+(define-metafunction λπ
+  subst : (x ..._1) (any ..._1) e -> e
+  [(subst (x ..._1) (any ..._1) e)
+   ,(subst/proc (redex-match? λπ x) (term (x ...)) (term (any ...)) (term e))])
 
 (define-term vnone (obj-val none (meta-none) ()))
 
