7cadd68fdd4aa4360dffe20b0e4222705204cd3a
Daniel Patterson <dbp@riseup.net>
2012-12-23 12:45:52 -0500

    redex: fixing bug for default term env, renaming expect-val to expect, and adding more control tests

--- redex/lambdapy-tests.rkt
+++ redex/lambdapy-tests.rkt
@@ -12,7 +12,7 @@
 
 
 (define (reduce-many e)
-  (define t (apply-reduction-relation* λπ-red (term (,e () ()))))
+  (define t (apply-reduction-relation* λπ-red (term (,e (()) ()))))
   (when (not (equal? (length t) 1))
     (error (format "Ambiguous reduction: ~a" t)))
   (define results (first t))
@@ -20,7 +20,7 @@
       (error (format "Resulted in non-term: ~a" (first t)))
       (first results)))
 
-(define (expect-val e v)
+(define (expect e v)
   (define result (reduce-many e))
   (if (test-equal result v)
       (void)
@@ -28,11 +28,26 @@
 
 
 ;; Primitive values
-(expect-val (term none) (term vnone))
-(expect-val (term true) (term vtrue))
-(expect-val (term false) (term vfalse))
-(expect-val (term undefined) (term undefined-val))
+(expect (term none) (term vnone))
+(expect (term true) (term vtrue))
+(expect (term false) (term vfalse))
+(expect (term undefined) (term undefined-val))
 
 ;; control flow
-(expect-val (term (if true none undefined)) (term vnone))
-(expect-val (term (if false none undefined)) (term undefined-val))
+(expect (term (if true none undefined)) (term vnone))
+(expect (term (if false none undefined)) (term undefined-val))
+(expect (term (if (exception-r vtrue) false false))
+	(term (exception-r vtrue)))
+(expect (term (seq true false)) (term vfalse))
+(expect (term (seq (exception-r vtrue) false))
+	(term (exception-r vtrue)))
+(expect (term (seq false (exception-r vtrue)))
+	(term (exception-r vtrue)))
+(expect (term (seq break false))
+	(term break-r))
+(expect (term (while true break false))
+	(term break-r))
+
+;; binding
+(expect (term (let (x vtrue) (id x local)))
+	(term vtrue))
