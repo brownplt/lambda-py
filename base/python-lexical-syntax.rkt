2cc7ee4080489bc650457dd24d25b352f2e66c2b
Matthew Milano <mpmilano@cs.brown.edu>
2013-01-05 12:13:56 -0500

    syntax implemented

--- base/python-lexical-syntax.rkt
+++ base/python-lexical-syntax.rkt
@@ -1,122 +1,81 @@
 #lang plai-typed
 
-#|
+(define-type PyExpr
+  ; control structures
+  [PyIf (test : PyExpr) (body : PyExpr) (orelse : PyExpr)]
+  [PySeq (es : (listof PyExpr))]
+  ; the top level seq
+  [PyModule (es : (listof PyExpr))]
+  [PyAssign (targets : (listof PyExpr)) (value : PyExpr)]
+  [PyAugAssign (op : symbol) (target : PyExpr) (value : PyExpr)]
 
-This is the core language; it is just borrowing a few things from 
-ParselTongue.
+  ; primitive literals
+  [PyNum (n : number)]
+  [PyBool (b : boolean)]
 
-|#
+  ; exceptions and exception handling
+  [PyRaise (expr : PyExpr)]
+  [PyExcept (types : (listof PyExpr)) (body : PyExpr)]
+  [PyExceptAs (types : (listof PyExpr)) (name : symbol) (body : PyExpr)]
+  [PyTryExceptElseFinally (try : PyExpr) (except : (listof PyExpr))
+                          (orelse : PyExpr) (finally : PyExpr)]
 
-(require (typed-in racket/base (number->string : (number -> string))))
-(require [opaque-type-in racket/set [Set set?]])
+  ;loops 
+  [PyWhile (test : PyExpr) (body : PyExpr) (orelse : PyExpr)]
+  [PyFor (target : PyExpr) (iter : PyExpr) (body : PyExpr)]
+  
+  ; pass
+  [PyPass]
 
-(define-type CExpr
-  [CStr (s : string)]
-  [CTrue]
-  [CFalse]
-  [CNone]
-  [CClass (name : symbol) (base : symbol) (body : CExpr)]
-  [CObject (class : symbol) (bval : (optionof MetaVal))]
-  [CGetField (value : CExpr) (attr : symbol)]
-  [CSeq (e1 : CExpr) (e2 : CExpr)]
-  [CAssign (target : CExpr) (value : CExpr)]
-  [CError (e1 : CExpr)]
-  [CIf (test : CExpr) (then : CExpr) (else : CExpr)]
-  [CId (x : symbol) (type : IdType)]
-  [CLet (x : symbol) (bind : CExpr) (body : CExpr)]
-  [CApp (fun : CExpr) (args : (listof CExpr)) (stararg : (optionof CExpr))]
-  [CFunc (args : (listof symbol)) (varargs : (optionof symbol)) (body : CExpr)
-         (method : boolean)]
-  [CWhile (test : CExpr) (body : CExpr) (orelse : CExpr)]
-  [CReturn (value : CExpr)]
-  [CPrim1 (prim : symbol) (arg : CExpr)]
-  [CPrim2 (prim : symbol) (arg1 : CExpr) (arg2 : CExpr)]
-  [CBuiltinPrim (op : symbol) (args : (listof CExpr))]
-  [CList (values : (listof CExpr))]
-  [CTuple (values : (listof CExpr))]
-  [CDict (contents : (hashof CExpr CExpr))]
-  [CSet (values : (listof CExpr))]
-  [CRaise (expr : (optionof CExpr))]
-  [CTryExceptElseFinally (try : CExpr) (excepts : (listof CExpr))
-                         (orelse : CExpr) (finally : CExpr)]
-  [CExcept (types : (listof CExpr)) (name : (optionof symbol)) (body : CExpr)]
-  [CUndefined]
-  [CBreak]
-  [CModule (prelude : CExpr) (body : CExpr)])
+  ; classes and objects 
+  [PyClass (name : symbol) (bases : (listof symbol)) (body : PyExpr)]
+  [PyDotField (value : PyExpr) (attr : symbol)]
 
-(define-type IdType
-    [GlobalId]
-    [NonlocalId]
-    [LocalId])
+  ; operations
+  [PyBinOp (left : PyExpr) (op : symbol) (right : PyExpr)] ;op = 'Add | 'Sub | etc
+  [PyUnaryOp (op : symbol) (operand : PyExpr)]
 
-(define-type IdPair
-    [idpair (id : symbol) (type : IdType)])
+  [PyCompOp (left : PyExpr) 
+            (ops : (listof symbol)) ;ops = 'Eq | 'NotEq | 'Lt etc
+            (comparators : (listof PyExpr))]
+  [PyBoolOp (op : symbol) (values : (listof PyExpr))] ;op = 'And | 'Or
 
-(define-type-alias IdEnv (listof IdPair))
+  ; functions
+  [PyLam (args : (listof symbol)) (body : PyExpr)]
+  [PyFunc (name : symbol) (args : (listof symbol)) (defaults : (listof PyExpr)) (body : PyExpr)]
+  [PyClassFunc (name : symbol) (args : (listof symbol)) (body : PyExpr)]
+  [PyFuncVarArg (name : symbol) (args : (listof symbol)) 
+                (sarg : symbol) (body : PyExpr)]
+  [PyReturn (value : PyExpr)]
+  [PyApp (fun : PyExpr) (args : (listof PyExpr))]
+  [PyAppStarArg (fun : PyExpr) (args : (listof PyExpr)) (stararg : PyExpr)]
 
-(define-type CVal
-  [VObject (antecedent : symbol) (mval : (optionof MetaVal)) (dict : object-dict)]
-  [VClosure (env : Env) (args : (listof symbol)) (vararg : (optionof symbol)) (body : CExpr)]
-  [VUndefined])
+  [PyDelete (targets : (listof PyExpr))]
 
-(define-type MetaVal
-             [MetaNum (n : number)]
-             [MetaStr (s : string)]
-             [MetaList (v : (listof CVal))]
-             [MetaTuple (v : (listof CVal))]
-             [MetaDict (contents : (hashof CVal CVal))]
-             [MetaClass (c : symbol)]
-             [MetaSet (elts : Set)]
-             [MetaNone])
+  ;
+  [PySubscript (left : PyExpr) (context : symbol) (slice : PyExpr)]
 
-;; env is a listof hashof's so there are deliniations between closures
-(define-type-alias Env (listof (hashof symbol Address)))
+  [PyListComp (body : PyExpr) (generators : (listof PyExpr))]
+  [PyComprehen (target : PyExpr) (iter : PyExpr)]
 
-(define-type-alias Address number)
-(define Address->string number->string)
-(define-type-alias Store (hashof Address CVal))
-(define new-loc
-  (let ([n (box 0)])
-    (lambda ()
-      (begin
-        (set-box! n (add1 (unbox n)))
-        (unbox n)))))
-
-(define-type Result
-  [v*s*e (v : CVal) (s : Store) (e : Env)]
-  [Return (v : CVal) (s : Store) (e : Env)]
-  [Exception (v : CVal) (s : Store) (e : Env)]
-  [Break (s : Store) (e : Env)])
-
-(define-type-alias object-dict (hashof symbol Address))
-
-(define (lookup [x : symbol] [env : Env]) : (optionof Address)
-  (cond
-    [(empty? env) (none)]
-    [else (type-case (optionof Address) (hash-ref (first env) x)
-            [some (v) (some v)]
-            [none () (lookup x (rest env))])]))
-
-;; lookup in just the local environment
-(define (lookup-local [x : symbol] [env : Env]) : (optionof Address)
-  (hash-ref (first env) x))
-
-;; lookup for nonlocal variables:
-;;   skip the current scope level
-;;   don't go to the global scope level
-(define (lookup-nonlocal [x : symbol] [env : Env]) : (optionof Address)
-  (local [(define rec-lookup-nonlocal
-            (λ ([x : symbol] [env : Env]) : (optionof Address)
-               (cond
-                 [(empty? (rest env)) (none)]
-                 [else (type-case (optionof Address) (hash-ref (first env) x)
-                         [some (v) (some v)]
-                         [none () (lookup x (rest env))])])))]
-    (rec-lookup-nonlocal x (rest env))))
+  ;new identifiers for scope.
+  [PyLocalId (x : symbol)]
+  [PyGlobalId (x : symbol)]
+  [PyInstanceId (x : symbol)]
+  [PyGlobalLet (id : symbol) (bind : PyExpr) (body : PyExpr)]
+  [PyLocalLet (id : symbol) (bind : PyExpr) (body : PyExpr)]
 
 
-(define (fetch [w : Address] [sto : Store]) : CVal
-  (type-case (optionof CVal) (hash-ref sto w)
-    [some (v) v]
-    [none () (error 'interp (string-append "No value at address " (Address->string w)))]))
+  ; builtin data structures
+  [PyStr (s : string)]
+  [PyDict (keys : (listof PyExpr)) (values : (listof PyExpr))]
+  [PyList (values : (listof PyExpr))]
+  [PySlice (lower : PyExpr) (upper : PyExpr) (step : PyExpr)]
+  [PyTuple (values : (listof PyExpr))]
+  [PyUndefined]
+  [PySet (elts : (listof PyExpr))]
+  [PyNone]
+  [PyBreak]
+
+)
 
