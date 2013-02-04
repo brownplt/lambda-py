671e9add64b075a280ab91e6df9539d3d976e054
Sumner Warren <sumner_warren@brown.edu>
2013-02-04 11:10:30 -0500

    Fix for shadowing bug. Needed call to lookup-global in assign-to-id. Was accidentally removed at some point.

--- base/python-interp.rkt
+++ base/python-interp.rkt
@@ -653,7 +653,10 @@
     )))
 
 (define (assign-to-id [id : CExpr] [val : Result] [env : Env] [sto : Store]) : Result
-  (local [(define mayb-loc (lookup (CId-x id) env))
+  (local [(define mayb-loc 
+            (type-case IdType (CId-type id)
+              [LocalId () (lookup (CId-x id) env)]
+              [GlobalId () (lookup-global (CId-x id) env)]))
           (define value (if (v*s? val)
                             (if (some? (v*s-a val))
                                 (VPointer (some-v (v*s-a val)))
