0c7838ed9850fd5f672211c1a6b5bc794619bfe2
Joe Gibbs Politz <joe@cs.brown.edu>
2013-03-21 00:50:02 -0400

    Reorganize some toplevel machinery
    - It wasn't possible to use the full interpreter from other directories,
      because of relative path issues.  Fixed with define-runtime-path
    
    - It wasn't possible to use desugar-w/libs and friends from other modules,
      b/c command-line would try to run.  Fixed with python-tools.rkt

--- base/python-main.rkt
+++ base/python-main.rkt
@@ -3,65 +3,11 @@
 (require (planet dherman/json:4:0))
 (require racket/cmdline
          racket/pretty
+         "python-tools.rkt"
          "parse-python.rkt"
-         "get-structured-python.rkt"
-         "python-interp.rkt"
-         "python-phases.rkt"
-         "python-desugar.rkt"
-         "python-cps.rkt"
-         "python-macros.rkt"
-         "python-lib.rkt"
          "run-tests.rkt"
-         "util.rkt"
          "python-evaluator.rkt")
 
-(define (python-test-runner _ port)
-  (run-python port))
-
-(define (run-python port)
-  (interp
-    (python-lib
-      (desugar-generators
-       (desugar
-        (new-scope-phase
-          (get-structured-python
-            (parse-python/port port (get-pypath)))))))))
-
-(define (get-surface-syntax port)
-  (get-structured-python
-   (parse-python/port port (get-pypath))))
-
-(define (get-lexical-syntax port)
-  (new-scope-phase
-   (get-structured-python
-    (parse-python/port port (get-pypath)))))
-
-(define (desugar-w/lex port)
-  (desugar
-    (new-scope-phase
-      (get-structured-python
-        (parse-python/port port (get-pypath))))))
-
-(define (desugar-w/lib port)
-  (python-lib
-   (desugar
-    (new-scope-phase
-     (get-structured-python
-      (parse-python/port port (get-pypath)))))))
-
-(define (desugar-w/macros port)
-  (desugar
-   (desugar-macros
-    (new-scope-phase
-     (get-structured-python
-      (parse-python/port port (get-pypath)))))))
-
-
-(define (get-core-syntax port)
-  (desugar
-    (new-scope-phase
-      (get-structured-python
-        (parse-python/port port (get-pypath))))))
 
 (command-line
   #:once-each
---
+++ base/python-tools.rkt
@@ -0,0 +1,65 @@
+#lang racket/base
+
+(require racket/pretty
+         "parse-python.rkt"
+         "get-structured-python.rkt"
+         "python-interp.rkt"
+         "python-phases.rkt"
+         "python-desugar.rkt"
+         "python-macros.rkt"
+         "python-lib.rkt"
+         "run-tests.rkt"
+         "util.rkt"
+         "python-evaluator.rkt")
+
+(provide (all-defined-out)
+         set-pypath
+         get-pypath)
+
+(define (python-test-runner _ port)
+  (run-python port))
+
+(define (run-python port)
+  (interp
+    (python-lib
+      (desugar
+        (new-scope-phase
+          (get-structured-python
+            (parse-python/port port (get-pypath))))))))
+
+(define (get-surface-syntax port)
+  (get-structured-python
+   (parse-python/port port (get-pypath))))
+
+(define (get-lexical-syntax port)
+  (new-scope-phase
+   (get-structured-python
+    (parse-python/port port (get-pypath)))))
+
+(define (desugar-w/lex port)
+  (desugar
+    (new-scope-phase
+      (get-structured-python
+        (parse-python/port port (get-pypath))))))
+
+(define (desugar-w/lib port)
+  (python-lib
+   (desugar
+    (new-scope-phase
+     (get-structured-python
+      (parse-python/port port (get-pypath)))))))
+
+(define (desugar-w/macros port)
+  (desugar
+   (desugar-macros
+    (new-scope-phase
+     (get-structured-python
+      (parse-python/port port (get-pypath)))))))
+
+
+(define (get-core-syntax port)
+  (desugar
+    (new-scope-phase
+      (get-structured-python
+        (parse-python/port port (get-pypath))))))
+
