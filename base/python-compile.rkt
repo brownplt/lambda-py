17bb34db48454369fb1d54b75a7850e860633618
Junsong Li <ljs.darkfish@gmail.com>
2013-03-22 15:45:09 +0800

    find globals using info provided in phase2 instead of stripping CLet in CModule body

--- base/python-compile.rkt
+++ base/python-compile.rkt
@@ -5,6 +5,7 @@
          "python-desugar.rkt"
          "util.rkt"
          (typed-in "parse-python.rkt" (parse-python/port : ('a 'b -> 'c)))
+         (typed-in "parse-python.rkt" (parse-python/string : ('a 'b -> 'c)))         
          (typed-in "get-structured-python.rkt"
                    (get-structured-python : ('a -> 'b)))   
          (typed-in racket/base (open-input-file : ('a -> 'b)))
@@ -20,14 +21,20 @@
   (compile-port
    (open-input-string str)))
 
-(define (get-global-names (es : CExpr)) : (listof symbol)
-  (type-case CExpr es
-    (CModule (pre body)
-             ;skip %locals trick
-             (get-global-names (CSeq-e2 body)))
-    (CLet (x type bind body)
-          (cons x (get-global-names body)))
-    (else (list))))
+;; (define (get-global-names (es : CExpr)) : (listof symbol)
+;;   (type-case CExpr es
+;;     (CModule (pre body)
+;;              ;skip %locals trick
+;;              (get-global-names (CSeq-e2 body)))
+;;     (CLet (x type bind body)
+;;           (cons x (get-global-names body)))
+;;     (else (list))))
+
+;; a more stable way to find globals in expr
+(define (get-global-names expr-str)
+  (get-module-level-globals
+   (get-structured-python
+    (parse-python/string expr-str (get-pypath)))))
 
 ;; built-in compile function, which takes
 ;; source, filename, mode and code class as its arguments
@@ -37,7 +44,7 @@
                       [filename (MetaStr-s mval2)]
                       [mode (MetaStr-s mval3)]
                       [code (compile-string source)]
-                      [globals (get-global-names code)]
+                      [globals (get-global-names source)]
                       [code-class (fourth args)])
                  (some (VObjectClass 'code
                                      (some (MetaCode code filename globals))
--- base/python-phases.rkt
+++ base/python-phases.rkt
@@ -4,3 +4,17 @@
 
 (define (new-scope-phase expr)
   (phase2 (scope-phase expr)))
+
+;; extract global bindings for module use 
+;; This is not efficient BTW
+;; -- Junsong
+(define (get-module-level-globals expr)
+  (extract-post-transform-globals
+   (remove-nonlocal
+    (remove-blocks
+     (make-local-list
+      empty
+      (collapse-pyseq
+       (post-desugar
+        (scope-phase expr))))))))
+
