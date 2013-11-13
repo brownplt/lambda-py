167916645eab587cfcd6bc691a24fbab19cb2403
Joe Gibbs Politz <joe@cs.brown.edu>
2013-11-13 17:24:12 -0500

    more examples

---
+++ tests/python-reference/scope/nonlocal-function-vardef.py
@@ -0,0 +1,17 @@
+def f():
+    x = 0
+    def inc():
+        nonlocal x
+        x += 1
+        return x
+    def dec():
+        nonlocal x
+        x -= 1
+        return x
+    return inc, dec
+
+inc, dec = f()
+___assertEqual(inc(), 1)
+___assertEqual(inc(), 2)
+___assertEqual(dec(), 1)
+___assertEqual(dec(), 0)
